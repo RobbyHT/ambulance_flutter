4872d6ca708bac96c79b34962105896dd7dcfb88
GiChing <10936027@ntub.edu.tw>
2021-03-19 09:11:54 +0800

    把原本的singup跟login的資料夾拉入Welcome的資料夾

---
+++ lib/screens/Welcom/Login2/login_screen.dart
@@ -0,0 +1,11 @@
+import 'package:ambulance_flutter/screens/Welcom/Login2/components/body.dart';
+import 'package:flutter/material.dart';
+
+class Login2Screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
