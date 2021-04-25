21dcd654cacfd7e32724e96bba98c84d08c9a0a4
GiChing <10936027@ntub.edu.tw>
2021-04-25 21:25:26 +0800

    新增管理者首頁的畫面dart

---
+++ lib/screens/Welcom/mangementWelcome/mangementWelcome_screen.dart
@@ -0,0 +1,13 @@
+
+import 'package:ambulance_flutter/screens/Welcom/mangementWelcome/m_body.dart';
+import 'package:flutter/material.dart';
+
+class MangementWelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(20,39,78,1),
+      body: m_Body(),
+    );
+  }
+}
