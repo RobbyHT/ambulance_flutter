614b3c3cb168490b940d447e868d09c956f7486f
judy0104 <10936029@ntub.edu.tw>
2021-07-10 00:56:51 +0800

    客戶管理首頁微調

--- lib/screens/clientdata/ClientData.dart
+++ lib/screens/clientdata/ClientData.dart
@@ -1,3 +1,4 @@
+import 'package:ambulance_flutter/screens/clientdata/clientonly.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
@@ -45,7 +46,7 @@ class ClientState extends State<ClientData> {
                 return Container(
                   height: 90,
                   width: double.infinity,
-                  margin: EdgeInsets.only(left: 60, right: 60, top: 25),
+                  margin: EdgeInsets.only(left: 50, right: 50, top: 25),
                   padding: EdgeInsets.symmetric(vertical: 10),
                   decoration: BoxDecoration(
                     color: Color.fromARGB(240, 155, 164, 181),
@@ -77,98 +78,15 @@ class ClientState extends State<ClientData> {
                       size: 30,
                       color: Colors.white,
                     ),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ClientOnlyScreen(client)));
+                    },
                   ),
                 );
-              })
-          /* Container(
-          height: 110,
-          margin: EdgeInsets.only(left: 40, right: 40, top: 25, bottom: 20),
-          decoration: BoxDecoration(
-            color: Color.fromARGB(170, 155, 164, 181),
-            borderRadius: BorderRadius.all(Radius.circular(15)),
-          ),
-          child: Column(
-            children: <Widget>[
-              ListView.builder(
-                  controller: controller,
-                  itemCount: itemsData.length,
-                  physics: BouncingScrollPhysics(),
-                  itemBuilder: (context, index) {
-                    return itemsData[index];
-                  }),
-            ],
-          ),
-        ),*/
-          ),
+              })),
     );
   }
-
-  /*ScrollController controller = ScrollController();
-  bool closeTopContainer = false;
-  double topContainer = 0;
-
-  List<Widget> itemsData = [];
-
-  void getPostsData() {
-    List<dynamic> responseList = CLIENT_DATA;
-    List<Widget> listItems = [];
-    responseList.forEach((post) {
-      listItems.add(Container(
-          height: 150,
-          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20.0)),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(color: Colors.black.withAlpha(100), blurRadius: 10.0),
-              ]),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      post["name"],
-                      style: const TextStyle(
-                          fontSize: 28, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      post["brand"],
-                      style: const TextStyle(fontSize: 17, color: Colors.grey),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                  ],
-                ),
-                ListTile(
-                  leading: Icon(Icons.person),
-                  trailing: Icon(Icons.keyboard_arrow_right),
-                ),
-              ],
-            ),
-          )));
-    });
-    setState(() {
-      itemsData = listItems;
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getPostsData();
-    controller.addListener(() {
-      double value = controller.offset / 119;
-
-      setState(() {
-        topContainer = value;
-        closeTopContainer = controller.offset > 50;
-      });
-    });
-  }*/
-
 }
