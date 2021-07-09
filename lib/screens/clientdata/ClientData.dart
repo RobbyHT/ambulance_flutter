c16832fad15905526464739f437663e170a73b4b
judy0104 <10936029@ntub.edu.tw>
2021-07-10 00:17:06 +0800

    完成客戶管理首頁

--- lib/screens/clientdata/ClientData.dart
+++ lib/screens/clientdata/ClientData.dart
@@ -13,7 +13,7 @@ class ClientState extends State<ClientData> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          backgroundColor: Color.fromARGB(147, 20, 39, 79),
+          backgroundColor: Color.fromARGB(160, 20, 39, 79),
           appBar: AppBar(
             elevation: 0,
             title: Text('顧客管理', style: TextStyle(fontSize: 20)),
@@ -21,9 +21,9 @@ class ClientState extends State<ClientData> {
             backgroundColor: Color.fromARGB(147, 57, 72, 103),
             leading: IconButton(
               icon: Icon(
-                Icons.keyboard_arrow_left,
+                Icons.arrow_back_ios_rounded,
                 color: Colors.white,
-                size: 40.0,
+                size: 25.0,
               ),
               onPressed: () {},
             ),
@@ -44,22 +44,39 @@ class ClientState extends State<ClientData> {
                 Clientdata client = CLIENT_DATA[index];
                 return Container(
                   height: 90,
+                  width: double.infinity,
                   margin: EdgeInsets.only(left: 60, right: 60, top: 25),
+                  padding: EdgeInsets.symmetric(vertical: 10),
                   decoration: BoxDecoration(
-                    color: Color.fromARGB(158, 155, 164, 181),
-                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    color: Color.fromARGB(240, 155, 164, 181),
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
                   ),
                   child: ListTile(
                     title: Text(
                       client.name,
-                      style: const TextStyle(fontSize: 20, color: Colors.white),
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
                     ),
                     subtitle: Text(
                       '電話  ' + client.phone,
-                      style: const TextStyle(fontSize: 16, color: Colors.white),
+                      style: const TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                        height: 2.0,
+                      ),
                     ),
-                    leading: Icon(Icons.person),
-                    trailing: Icon(Icons.keyboard_arrow_right),
+                    leading: Icon(
+                      Icons.person,
+                      size: 50,
+                      color: Colors.white,
+                    ),
+                    trailing: Icon(
+                      Icons.arrow_forward_ios_rounded,
+                      size: 30,
+                      color: Colors.white,
+                    ),
                   ),
                 );
               })
