e0477fef179eeabe4b01275d771de5740e8b3556
judy0104 <10936029@ntub.edu.tw>
2021-07-09 23:41:43 +0800

    更新暫時資料庫

--- lib/screens/clientdata/client_data.dart
+++ lib/screens/clientdata/client_data.dart
@@ -1,22 +1,19 @@
-const CLIENT_DATA = [
-  {
-    "name": "朱雅筠",
-    "phone": "09123456",
-  },
-  {
-    "name": "謝咏螢",
-    "phone": "09123456",
-  },
-  {
-    "name": "林昊霆",
-    "phone": "09123456",
-  },
-  {
-    "name": "謝欣汝",
-    "phone": "09123456",
-  },
-  {
-    "name": "洪季晴",
-    "phone": "09123456",
-  },
+import 'package:flutter/cupertino.dart';
+
+class Clientdata {
+  String name;
+  String phone;
+
+  Clientdata({
+    @required this.name,
+    @required this.phone,
+  });
+}
+
+List<Clientdata> CLIENT_DATA = [
+  Clientdata(name: '林昊霆', phone: '09123456'),
+  Clientdata(name: '謝欣汝', phone: '09123456'),
+  Clientdata(name: '洪季晴', phone: '09123456'),
+  Clientdata(name: '謝咏螢', phone: '09123456'),
+  Clientdata(name: '朱雅筠', phone: '09123456'),
 ];
