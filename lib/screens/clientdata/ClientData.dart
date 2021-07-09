ee1251a789b0d030a45d941a51ff57f79ecc04fa
judy0104 <10936029@ntub.edu.tw>
2021-07-09 23:43:19 +0800

    客戶格

--- lib/screens/clientdata/ClientData.dart
+++ lib/screens/clientdata/ClientData.dart
@@ -1,57 +1,157 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart';
 import 'client_data.dart';
 
 class ClientData extends StatefulWidget {
-  ClientData({Key key}) : super(key: key);
-
+  @override
   ClientState createState() => ClientState();
 }
 
 class ClientState extends State<ClientData> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(147, 20, 39, 79),
-      appBar: new AppBar(
-        elevation: 0,
-        title: Text('顧客管理', style: TextStyle(fontSize: 20)),
-        backgroundColor: Color.fromARGB(147, 57, 72, 103),
-        centerTitle: true,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search, color: Colors.white),
-            onPressed: () {},
-          )
-        ],
-      ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: ListView.builder(
-                itemCount: clientsdata.length,
-                physics: BouncingScrollPhysics(),
-                itemBuilder: (context, index) {
-                  return clientsdata[index];
-                },
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: Color.fromARGB(147, 20, 39, 79),
+          appBar: AppBar(
+            elevation: 0,
+            title: Text('顧客管理', style: TextStyle(fontSize: 20)),
+            centerTitle: true,
+            backgroundColor: Color.fromARGB(147, 57, 72, 103),
+            leading: IconButton(
+              icon: Icon(
+                Icons.keyboard_arrow_left,
+                color: Colors.white,
+                size: 40.0,
               ),
-            )
-          ],
-        ),
-      ),
+              onPressed: () {},
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(
+                  Icons.search,
+                  color: Colors.white,
+                  size: 35.0,
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          body: ListView.builder(
+              itemCount: CLIENT_DATA.length,
+              itemBuilder: (context, index) {
+                Clientdata client = CLIENT_DATA[index];
+                return Container(
+                  height: 90,
+                  margin: EdgeInsets.only(left: 60, right: 60, top: 25),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(158, 155, 164, 181),
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                  child: ListTile(
+                    title: Text(
+                      client.name,
+                      style: const TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                    subtitle: Text(
+                      '電話  ' + client.phone,
+                      style: const TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                    leading: Icon(Icons.person),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                  ),
+                );
+              })
+          /* Container(
+          height: 110,
+          margin: EdgeInsets.only(left: 40, right: 40, top: 25, bottom: 20),
+          decoration: BoxDecoration(
+            color: Color.fromARGB(170, 155, 164, 181),
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+          ),
+          child: Column(
+            children: <Widget>[
+              ListView.builder(
+                  controller: controller,
+                  itemCount: itemsData.length,
+                  physics: BouncingScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return itemsData[index];
+                  }),
+            ],
+          ),
+        ),*/
+          ),
     );
   }
 
-  List<Widget> clientsdata = [];
+  /*ScrollController controller = ScrollController();
+  bool closeTopContainer = false;
+  double topContainer = 0;
+
+  List<Widget> itemsData = [];
 
   void getPostsData() {
     List<dynamic> responseList = CLIENT_DATA;
     List<Widget> listItems = [];
     responseList.forEach((post) {
       listItems.add(Container(
-        height: 150,
-      ));
+          height: 150,
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20.0)),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(color: Colors.black.withAlpha(100), blurRadius: 10.0),
+              ]),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      post["name"],
+                      style: const TextStyle(
+                          fontSize: 28, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      post["brand"],
+                      style: const TextStyle(fontSize: 17, color: Colors.grey),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+                ListTile(
+                  leading: Icon(Icons.person),
+                  trailing: Icon(Icons.keyboard_arrow_right),
+                ),
+              ],
+            ),
+          )));
+    });
+    setState(() {
+      itemsData = listItems;
     });
   }
+
+  @override
+  void initState() {
+    super.initState();
+    getPostsData();
+    controller.addListener(() {
+      double value = controller.offset / 119;
+
+      setState(() {
+        topContainer = value;
+        closeTopContainer = controller.offset > 50;
+      });
+    });
+  }*/
+
 }
