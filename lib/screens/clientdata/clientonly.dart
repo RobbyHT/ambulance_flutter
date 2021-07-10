0468747b50b0fbad17b7be6b7b3bb285e9bd46a6
judy0104 <10936029@ntub.edu.tw>
2021-07-10 19:00:45 +0800

    客戶管理單一頁面完成，差可修改的文字框內容並連接資料庫

--- lib/screens/clientdata/clientonly.dart
+++ lib/screens/clientdata/clientonly.dart
@@ -1,10 +1,10 @@
 import 'package:ambulance_flutter/screens/clientdata/ClientData.dart';
 import 'package:ambulance_flutter/screens/clientdata/client_data.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 
 class ClientOnlyScreen extends StatelessWidget {
   final Clientdata client;
+  final myController = TextEditingController();
 
   ClientOnlyScreen(this.client);
 
@@ -37,7 +37,7 @@ class ClientOnlyScreen extends StatelessWidget {
             width: 350,
             decoration: BoxDecoration(
               color: Color.fromARGB(210, 206, 221, 240),
-              border: Border.all(width: 4, color: Colors.white),
+              border: Border.all(width: 5, color: Colors.white),
               borderRadius: BorderRadius.all(Radius.circular(30)),
             ),
           ),
@@ -51,8 +51,8 @@ class ClientOnlyScreen extends StatelessWidget {
                 client.name,
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w900,
+                  fontSize: 21,
+                  fontWeight: FontWeight.bold,
                   color: Color.fromARGB(220, 20, 39, 79),
                 ),
               ),
@@ -69,8 +69,21 @@ class ClientOnlyScreen extends StatelessWidget {
               '電話',
               style: TextStyle(
                   fontSize: 16,
-                  fontWeight: FontWeight.w800,
+                  fontWeight: FontWeight.bold,
                   color: Color.fromARGB(220, 20, 39, 79)),
+            ),
+          ),
+          Positioned(
+            top: 242,
+            left: 170,
+            child: Text(
+              client.phone,
+              style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(220, 20, 39, 79),
+                  decoration: TextDecoration.underline,
+                  decorationColor: Color.fromARGB(230, 20, 39, 79)),
             ),
           ),
           Positioned(
@@ -80,8 +93,22 @@ class ClientOnlyScreen extends StatelessWidget {
               '地址',
               style: TextStyle(
                   fontSize: 16,
-                  fontWeight: FontWeight.w800,
+                  fontWeight: FontWeight.bold,
                   color: Color.fromARGB(220, 20, 39, 79)),
+            ),
+          ),
+          Positioned(
+            top: 270,
+            left: 170,
+            child: Text(
+              client.address,
+              maxLines: 2,
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(220, 20, 39, 79),
+                  decoration: TextDecoration.underline,
+                  decorationColor: Color.fromARGB(230, 20, 39, 79)),
             ),
           ),
           Positioned(
@@ -91,8 +118,21 @@ class ClientOnlyScreen extends StatelessWidget {
               '優惠價',
               style: TextStyle(
                   fontSize: 16,
-                  fontWeight: FontWeight.w800,
+                  fontWeight: FontWeight.bold,
                   color: Color.fromARGB(220, 20, 39, 79)),
+            ),
+          ),
+          Positioned(
+            top: 300,
+            left: 170,
+            child: Text(
+              client.discount,
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(220, 20, 39, 79),
+                  decoration: TextDecoration.underline,
+                  decorationColor: Color.fromARGB(230, 20, 39, 79)),
             ),
           ),
           Positioned(
@@ -102,8 +142,48 @@ class ClientOnlyScreen extends StatelessWidget {
               '備註',
               style: TextStyle(
                   fontSize: 16,
-                  fontWeight: FontWeight.w800,
+                  fontWeight: FontWeight.bold,
                   color: Color.fromARGB(220, 20, 39, 79)),
+            ),
+          ),
+          Positioned(
+            top: 330,
+            left: 170,
+            child: Text(
+              client.other,
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(220, 20, 39, 79),
+                  decoration: TextDecoration.underline,
+                  decorationColor: Color.fromARGB(230, 20, 39, 79)),
+            ),
+          ),
+          Positioned(
+            top: 420,
+            left: 100,
+            child: IconButton(
+              icon: Icon(
+                Icons.cancel_rounded,
+                color: Colors.white,
+                size: 60,
+              ),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => ClientData()));
+              },
+            ),
+          ),
+          Positioned(
+            top: 420,
+            left: 230,
+            child: IconButton(
+              icon: Icon(
+                Icons.check_circle_rounded,
+                color: Colors.white,
+                size: 60,
+              ),
+              onPressed: () {},
             ),
           ),
         ]),
