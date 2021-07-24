b6341bac3fda3b30da39d0b1c943c5f2502b2313
GiChing <10936027@ntub.edu.tw>
2021-07-24 14:11:07 +0800

    建立設備管理UI資料夾

---
+++ lib/screens/equipmentUI/equipmentUI.dart
@@ -0,0 +1,11 @@
+import 'package:ambulance_flutter/screens/equipmentUI/components/eUI_body.dart';
+import 'package:flutter/material.dart';
+
+class equipmentUI extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: eUI_body(),
+    );
+  }
+}
