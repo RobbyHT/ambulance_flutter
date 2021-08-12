bd6d04e4839f438afb88de5a0ceb181cca01c7fc
GiChing <10936027@ntub.edu.tw>
2021-08-12 20:44:56 +0800

    建立appbar及跳頁

--- lib/screens/equipmentUI/equipmentUI.dart
+++ lib/screens/equipmentUI/equipmentUI.dart
@@ -1,11 +1,29 @@
-import 'package:ambulance_flutter/screens/equipmentUI/components/eUI_body.dart';
+import 'package:ambulance_flutter/screens/equipmentUI/components/choice.dart';
 import 'package:flutter/material.dart';
 
 class equipmentUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: eUI_body(),
+      appBar: AppBar(
+          backgroundColor: (Color.fromRGBO(57,72,103,100)) ,
+          centerTitle: true,
+          title: Text("設備管理"),
+          leading:
+          //返回上一頁 
+          IconButton(
+            icon:Icon(Icons.arrow_back_ios_rounded, color: Colors.white),
+            onPressed: (){},
+          ),
+          //搜尋
+          // actions: <Widget>[
+          //   IconButton(
+          //     icon: Icon(Icons.search_rounded , color: Colors.white),
+          //     onPressed: () {},
+          //     )
+          //     ]
+           ),      
+      body: Iphone11promainWidget(),
     );
   }
 }
