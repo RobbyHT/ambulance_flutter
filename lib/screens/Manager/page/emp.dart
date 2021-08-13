f44003c8b05ffd5764eb0a769a4050a355c709d8
XieXinRu <10936009@ntub.edu.tw>
2021-08-13 10:22:46 +0800

    建立員工管理介面

--- lib/screens/Manager/page/emp.dart
+++ lib/screens/Manager/page/emp.dart
@@ -1,40 +1,158 @@
 import 'package:flutter/material.dart';
+import '../components/empData.dart';
 
-class EmpPage extends StatelessWidget {
+class EmpPage extends StatefulWidget {
+@override
+  _empPageState createState() => _empPageState();
+}
+
+class _empPageState extends State<EmpPage> {
+  ScrollController controller = ScrollController();
+  bool closeTopContainer = false;
+  double topContainer = 0;
+
+  List<Widget> itemsData = [];
+
+  void getPostsData() {
+    List<dynamic> responseList = USER_DATA;
+    List<Widget> listItems = [];
+    responseList.forEach((post) {
+      listItems.add(Container(
+          height: 150,
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20.0)), color: Colors.white, boxShadow: [
+            BoxShadow(color: Colors.black.withAlpha(100), blurRadius: 10.0),
+          ]),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    IconButton(
+                      icon:Icon(Icons.person_rounded),
+                      iconSize: 60,
+                    ),
+                    Text(
+                      post["position"],
+                      style: const TextStyle(fontSize: 18, color: Colors.black),
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      post["name"],
+                      style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "id:${post["id"]}",
+                      style: const TextStyle(fontSize: 18, color: Colors.black),
+                    ),
+                    // SizedBox(
+                    //   height: 10,
+                    // ),
+                  ],
+                ),
+                SizedBox(
+                      width: 30,
+                ),
+                IconButton(
+                  icon:Icon(Icons.keyboard_arrow_right),
+                  iconSize: 60,
+                  onPressed: () {},
+                )
+              ],
+            ),
+          )));
+    });
+    setState(() {
+      itemsData = listItems;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getPostsData();
+    controller.addListener(() {
+
+      double value = controller.offset/119;
+
+      setState(() {
+        topContainer = value;
+        closeTopContainer = controller.offset > 50;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    //定义列表widget的list
-    List<Widget> list=<Widget>[];
-
-    //Demo数据定义
-    var data=[
-      {"id":1,"title":"测试数据AAA","subtitle":"ASDFASDFASDF"},
-      {"id":2,"title":"测试数据bbb","subtitle":"ASDFASDFASDF"},
-      {"id":3,"title":"测试数据ccc","subtitle":"ASDFASDFASDF"},
-      {"id":4,"title":"测试数据eee","subtitle":"ASDFASDFASDF"},
-    ];
-
-    //根据Demo数据，构造列表ListTile组件list
-    for (var item in data) {
-      print(item["title"]);
-
-      list.add( ListTile( 
-          title: Text(item["title"],style: TextStyle(fontSize: 18.0) ),
-          subtitle: Text(item["subtitle"]),
-          leading:  Icon( Icons.fastfood, color:Colors.orange ),
-          trailing: Icon(Icons.keyboard_arrow_right)
-      ));
-    }
-
-    //返回整个页面
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("員工管理"),
-      ),
-      body: Center(
-        child: ListView(
-          children: list,
-        )
+    final Size size = MediaQuery.of(context).size;
+    final double categoryHeight = size.height*0.30;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color.fromRGBO(20, 39, 78, 1),
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Color.fromRGBO(249, 244, 241, 1),
+          leading: Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search, color: Colors.black),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.settings, color: Colors.black),
+              onPressed: () {},
+            )
+          ],
+        ),
+        body: Container(
+          height: size.height,
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                  child: ListView.builder(
+                    controller: controller,
+                      itemCount: itemsData.length,
+                      physics: BouncingScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        double scale = 1.0;
+                        if (topContainer > 0.5) {
+                          scale = index + 0.5 - topContainer;
+                          if (scale < 0) {
+                            scale = 0;
+                          } else if (scale > 1) {
+                            scale = 1;
+                          }
+                        }
+                        return Opacity(
+                          opacity: scale,
+                          child: Transform(
+                            transform:  Matrix4.identity()..scale(scale,scale),
+                            alignment: Alignment.bottomCenter,
+                            child: Align(
+                                heightFactor: 0.7,
+                                alignment: Alignment.topCenter,
+                                child: itemsData[index]),
+                          ),
+                        );
+                      })),
+            ],
+          ),
+        ),
       ),
     );
   }
