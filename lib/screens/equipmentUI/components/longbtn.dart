6d26981a3832f470da6b4f277fc5b061811e8ef6
GiChing <10936027@ntub.edu.tw>
2021-08-14 18:24:53 +0800

    修改元件程式(長)

--- lib/screens/equipmentUI/components/longbtn.dart
+++ lib/screens/equipmentUI/components/longbtn.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 
-        class LongWidget extends StatefulWidget {
-        @override
-        _LongWidgetState createState() => _LongWidgetState();
-        }
-        
-        class _LongWidgetState extends State<LongWidget> {
-        @override
+class LongWidget extends StatefulWidget{
+  final String inputText;
+  final String number;
+  final String day;
+
+  const LongWidget({Key key, this.inputText, this.number, this.day}) : super(key: key);
+
+
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return LongWidgetState();
+  }
+} 
+class LongWidgetState extends State<LongWidget> {
+
+    @override
         Widget build(BuildContext context) {
-        // Figma Flutter Generator LongWidget - COMPONENT
-        
+      
      return Container(
       width: 300,
       height: 60,
@@ -21,7 +31,7 @@ import 'package:flutter/material.dart';
         Column(crossAxisAlignment: CrossAxisAlignment.start,children: <Widget>[
         Container(
             padding:EdgeInsets.only(left: 20,top:5),
-            child: Text("可摺疊式搬運椅/椅式擔架", style: TextStyle(
+            child: Text(this.widget.inputText, style: TextStyle(
         color: Color.fromRGBO(255, 255, 255, 1),
         fontSize: 16,),
           ),
@@ -36,7 +46,7 @@ import 'package:flutter/material.dart';
           ),
           Container(
           padding:EdgeInsets.only(left:20,bottom:0),
-          child:Text('2021/08/31',textAlign: TextAlign.left, style: TextStyle(
+          child:Text(this.widget.day,textAlign: TextAlign.left, style: TextStyle(
         color: Color.fromRGBO(255, 255, 255, 1),
         fontSize: 16,),) ,
           ),
@@ -44,10 +54,10 @@ import 'package:flutter/material.dart';
       ],
       ) , ),),
       Expanded(flex: 1,child:Container(width: 20,height: 30,
-        child:Text('1', textAlign: TextAlign.center, style: TextStyle(
+        child:Text(this.widget.number, textAlign: TextAlign.center, style: TextStyle(
         color: Color.fromRGBO(255, 255, 255, 1),
         fontSize: 25,)) ) ,)
       ],)      
       );
         }
-        }
+  }
