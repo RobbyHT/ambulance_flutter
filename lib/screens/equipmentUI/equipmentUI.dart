7d0a1d782ddb5fb65ed000dc2683626248262e35
GiChing <10936027@ntub.edu.tw>
2021-08-14 18:23:07 +0800

    改為tabbar

--- lib/screens/equipmentUI/equipmentUI.dart
+++ lib/screens/equipmentUI/equipmentUI.dart
@@ -1,29 +1,41 @@
-import 'package:ambulance_flutter/screens/equipmentUI/components/choice.dart';
+import 'package:ambulance_flutter/screens/equipmentUI/components/box_equipment.dart';
+import 'package:ambulance_flutter/screens/equipmentUI/components/car_equipment.dart';
+
 import 'package:flutter/material.dart';
 
 class equipmentUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          backgroundColor: (Color.fromRGBO(57,72,103,100)) ,
+     final List<Tab> myTabs = <Tab>[
+    Tab(text: '車內設備'),
+    Tab(text: '急救箱配備'),
+  ];
+
+  final pages = [CarWidget(), BoxWidget()];
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,//移除debug的標籤
+      home:DefaultTabController(length: myTabs.length,
+      child: Scaffold(appBar: AppBar(
+          backgroundColor: (Color.fromRGBO(57, 72, 103, 1)) ,
           centerTitle: true,
           title: Text("設備管理"),
+          bottom: TabBar(tabs:myTabs,indicatorColor: Color.fromRGBO(155,164,180, 5)),
           leading:
           //返回上一頁 
           IconButton(
             icon:Icon(Icons.arrow_back_ios_rounded, color: Colors.white),
             onPressed: (){},
           ),
-          //搜尋
-          // actions: <Widget>[
-          //   IconButton(
-          //     icon: Icon(Icons.search_rounded , color: Colors.white),
-          //     onPressed: () {},
-          //     )
-          //     ]
-           ),      
-      body: Iphone11promainWidget(),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              CarWidget(), BoxWidget()
+              ],
+          ),) ,),
+      
+              
+      
     );
+
   }
 }
